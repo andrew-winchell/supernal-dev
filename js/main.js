dd28b0df37cdbc13c2eacaebb7e6aafb6d51c8af
Andrew Winchell <awinchell@cobec.com>
2023-05-24 23:08:33 -0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+require([
+    "esri/portal/Portal",
+    "esri/identity/OAuthInfo",
+    "esri/identity/IdentityManager",
+    "esri/portal/PortalQueryParams",
+    "esri/WebScene",
+    "esri/views/SceneView",
+    "esri/widgets/ElevationProfile"
+], (Portal, OAuthInfo, esriId, PortalQueryParams, WebScene, SceneView, ElevationProfile) => {
+
+    // Esri AGOL Authorization
+    const info = new OAuthInfo({
+        appId: "VciB4pGjVFKkH1Og",
+        portalUrl: "https://cobecconsulting.maps.arcgis.com",
+        authNamespace: "portal_oauth_inline",
+        flowType: "auto",
+        popup: false
+    });
+    esriId.registerOAuthInfos([info]);
+    esriId.getCredential(info.portalUrl + "/sharing");
+    esriId.checkSignInStatus(info.portalUrl + "/sharing")
+        .then(() => {
+            console.log("Sign In Successful.");
+        }).catch(() => {
+            console.log("User not signed in.")
+        });
+
+    const webscene = new WebScene({
+        portalItem: { id: "d6127f4244ba4c838e726a7c0c1efe40" }
+    });
+});
