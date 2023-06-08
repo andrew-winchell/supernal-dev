9f528e7c6e8c28bd769aba9d37988a2447e62754
Andrew Winchell <awinchell@cobec.com>
2023-06-08 08:20:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@ require([
             mapView.ui.remove(sketch);
         } else if (evt.toolEventInfo.type === "vertex-add") {
             selectVertice(evt.graphic.geometry.paths[0])
-            console.log(evt.graphic.geometry.paths[0],evt.graphic.geometry.paths[0][0])
+            console.log(evt.graphic.geometry.paths[0],evt.graphic.geometry.paths[0].slice(-1))
         }
     })
 
