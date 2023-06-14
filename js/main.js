ef635e740bece0b725777b875765e492679c5c94
Andrew Winchell <awinchell@cobec.com>
2023-06-14 17:05:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -536,7 +536,7 @@ require([
         action.on("vertex-add", (evt) => {
             let altitude = prompt("Enter Altitude:");
             console.log(evt.vertices);
-            let point3D = screenToMap(evt.vertices)
+            let point3D = action.screenToMap();
             console.log(point3D)
             //createVertice(evt.vertices, altit)
         });
