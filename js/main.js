2d47c790c5da79c4bcc7bd3dc5ef67d0081d6c26
Andrew Winchell <awinchell@cobec.com>
2023-06-18 08:18:14 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -610,16 +610,11 @@ require([
                 multipointVertices.push(coords);
                 createVertice(multipointVertices);
             } else if (evt.state == "active") {
-                console.log(evt)
                 if (evt.toolEventInfo.type == "vertex-add") {
                     let altitude = prompt("Enter Altitude:", 0);
                     let coords = [evt.toolEventInfo.added[0][0], evt.toolEventInfo.added[0][1], parseInt(altitude)];
                     multipointVertices.push(coords);
                     createVertice(multipointVertices);
-                    drawPath(multipointVertices);
-                } else if (evt.toolEventInfo.type == "cursor-update") {
-                    let coords = [evt.toolEventInfo.added[0][0], evt.toolEventInfo.added[0][1]];
-                    multipointVertices.push(coords);
                     drawPath(multipointVertices);
                 }
             }
