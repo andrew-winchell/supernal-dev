a6f47d8be39f67698b11582043da2bc54881d768
Andrew Winchell <awinchell@cobec.com>
2023-06-17 23:28:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -594,7 +594,7 @@ require([
         pointSketchViewModel.create("multipoint");
 
         pointSketchViewModel.on("create", (evt) => {
-            if (evt.toolEventInfo.added) {
+            if (evt.toolEventInfo.added != null) {
                 console.log(evt)
             } else {
                 console.log(evt)
