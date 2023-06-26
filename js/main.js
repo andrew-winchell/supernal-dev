c3c1f562c0b11b4e2ef512b080183d3a30c02ad4
Andrew Winchell <awinchell@cobec.com>
2023-06-26 16:29:15 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -760,7 +760,7 @@ require([
             let y = mapPt.latitude.toFixed(4);
             let z = mapPt.z;
             $("#waypoints").append(
-                "<calcite-list-item disabled style='opacity:1;' label='Vertice #" + (i+1) + "' description='X: " + x + " Y: " + y +" Z: " + z + "'><calcite-list-item>"
+                "<calcite-list-item disabled style='opacity:1;' label='Vertice #" + (i+1) + "' description='X: " + x + " Y: " + y +" Z: " + z + "'></calcite-list-item>"
             )
         }
     }    
