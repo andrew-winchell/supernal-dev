ee143cbe39ebe1e6e4b71210e99b77ff75be7bbf
Andrew Winchell <awinchell@cobec.com>
2023-06-30 08:32:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1211,10 +1211,12 @@ require([
     function to2DView () {
         mapView.container = "view-div";
         sceneView.container = "inset-div";
+        $("#switch-btn")[0].value = "2D";
     }
 
     function to3DView () {
         mapView.container = "inset-div";
         sceneView.container = "view-div";
+        $("#switch-btn")[0].value = "3D";
     }
 });
