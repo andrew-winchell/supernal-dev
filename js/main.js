9af410d27b840a0eff0cff66b0cf0b710a406339
Andrew Winchell <awinchell@cobec.com>
2023-07-11 10:22:42 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -667,7 +667,7 @@ require([
         console.log(parseFloat(evt.target.textContent));
         $("#waypoint-table tr td").each((item)=>{
             if (item % 4 != 0) {
-                let cellVal = $(this);
+                let cellVal = $(this)[0];
                 console.log(cellVal)
             }
         })
