7ef37a56ee6a5a30b2553f8e684e7aece2311cae
Andrew Winchell <awinchell@cobec.com>
2023-07-11 11:17:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -665,7 +665,7 @@ require([
 
     $("#waypoint-table").on("input", (evt) => {
         console.log(parseFloat(evt.target.textContent));
-        $("#waypoint-table tr td").each((item) => {
+        $("#waypoint-table tbody tr td").each((item) => {
             let long = $(this).find("td").eq(1);
             let lat = $(this).find("td").eq(2).text();
             let alt = $(this).find("td").eq(3);
