46d67b9f9068efe68090db1c13eb5cb7efd885bb
Andrew Winchell <awinchell@cobec.com>
2023-07-18 16:15:21 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1229,7 +1229,7 @@ require([
                             $("#confirm-route").css("display", "block");
                             selectedFeatureTable(selectedFeature.geometry.paths);
                             selectedFeatureProfile(selectedFeature.geometry.paths);
-                            mapView.popup.container = "popup-container";
+                            mapView.popup.container = $("#popup-container");
                             /*mapView.popup.dockEnabled = true;
                             mapView.popup.set("dockOptions", {
                                 position: "bottom-right",
