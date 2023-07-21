cb980fc882168ba714e3bb94712f9de342d34475
Andrew Winchell <awinchell@cobec.com>
2023-07-20 20:53:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1529,29 +1529,6 @@ require([
       }
     );
     
-    // If popup is closed via X button
-    reactiveUtils.when(
-        () => mapView.popup,
-        "trigger-action",
-        (event) => {
-            console.log(event);
-        }
-        /*() => !mapView.popup?.visible,
-        () => {
-            // Hide any routes
-            supernalRoutesLyr.definitionExpression = "1=0";
-            // Zoom to US center
-            mapView.goTo({
-                center: [-98.583333 ,39.833333],
-                scale: 50000000
-            });
-            // Close Elevation Profile
-            elevationProfile.input = null;
-            // Close Vertices Table
-            $("#waypoint-list").css("display", "none");
-        }*/
-    );
-    
     supernalRoutesLyr.on("apply-edits", () => {
         mapView.ui.remove(editor);
         editor.viewModel.cancelWorkflow();
