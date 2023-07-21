6c7085154d6739c51ec79fbcf4df9698664ec91b
Andrew Winchell <awinchell@cobec.com>
2023-07-21 17:23:49 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1563,18 +1563,21 @@ require([
     );
     
     supernalRoutesLyr.on("apply-edits", (evt) => {
-        console.log(evt);
-        mapView.ui.remove(editor);
         editor.viewModel.cancelWorkflow();
-        drawPath(selectedFeature.geometry.paths);
-        selectedFeatureTable(selectedFeature.geometry.paths);
 
-        // Delete the current list of existing routes
-        $("#existing-routes").empty();
-        // Repopulate existing routes list with new values after 1 second delay
-        setTimeout(()=> {
-            populateExistingRoutes();
-        }, 1000);
+        if (event.edits.addFeatures) {
+            // Delete the current list of existing routes
+            $("#existing-routes").empty();
+            // Repopulate existing routes list with new values after 1 second delay
+            setTimeout(()=> {
+                populateExistingRoutes();
+            }, 1000);
+        } else {
+            console.log(evt);
+            mapView.ui.remove(editor);
+            drawPath(selectedFeature.geometry.paths);
+            selectedFeatureTable(selectedFeature.geometry.paths);
+        }
     });
 
 
