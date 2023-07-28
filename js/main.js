9cb0a65f8d87fec3b5f3c5f3b732e4d342d99e5d
Andrew Winchell <awinchell@cobec.com>
2023-07-28 08:58:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1368,7 +1368,7 @@ require([
 
     }
 
-    $("#existing-routes-list").on("calciteListChange", (evt) => {
+    $("#existing-routes").on("calciteListChange", (evt) => {
         console.log(evt)
         let objectIds =  [];
         for (let listItem of evt.currentTarget.selectedItems) {
