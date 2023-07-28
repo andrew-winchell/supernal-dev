7d5c73064c07cd226cbd4c85512f21abf078048d
Andrew Winchell <awinchell@cobec.com>
2023-07-28 17:44:07 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1402,7 +1402,7 @@ require([
         supernalRoutesLyr.queryFeatures(query)
             .then((results) => {
                 let selectedFeatures = results.features;
-                console.log(supernalRoutesLyr);
+                console.log(selectedFeatures);
                 mapView
                     .when(() => {
                         featuresWidget.features = selectedFeatures;
