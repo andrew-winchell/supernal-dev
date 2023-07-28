0b06b35591cbaf8e1a6a32536a237f6e344a673c
Andrew Winchell <awinchell@cobec.com>
2023-07-28 18:35:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -478,8 +478,7 @@ require([
                     ]
                 }
             ]
-        },
-        definitionExpression: "1=0"
+        }
     });
 
     /********** Client-Side Graphics Layers for Drawing **********/
