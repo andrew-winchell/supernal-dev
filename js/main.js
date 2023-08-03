5d0bd1f306abb9a8449a3d4d4cd9f2d1b772bfdf
Andrew Winchell <awinchell@cobec.com>
2023-08-03 17:10:17 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1600,6 +1600,7 @@ require([
       }
     );
     
+    /*
     supernalRoutesLyr.on("apply-edits", (evt) => {
         if (evt.edits.addFeatures) {
             // Delete the current list of existing routes
@@ -1622,6 +1623,7 @@ require([
             }, 10);
         }
     });
+    */
 
 
     /********** Elevation Profile Widget **********/
